19349011e6079962725609b6f362e3c6b65a595e
gitdiogenes <robert.keyser@gmail.com>
2017-02-27 23:53:08 +0000

    fartsman

--- mhscript.js
+++ mhscript.js
@@ -1,32 +1,33 @@
 
 //  do farts = a document.getElementsByName('name_value')
       //then on farts.addEventListener("click", function "change .src")
-var cur = 0;   
-var productSliderSRC = ["https://dummyimage.com/300.png/09f/fff"+new Date().getTime(),"https://dummyimage.com/200x300&text=dummyimage.com+rocks!"+new Date().getTime(),"http://lorempixel.com/400/200/sports/1","5","5"];   
+var cur = 0;  
       
 var touchstartX = 0;
 var touchstartY = 0;
 var touchendX = 0;
 var touchendY = 0;
 var swipedir ;
-
 var gesturedZone = document.getElementById('slider-container');
-var board = document.getElementsByClassName("slider-board");
+var sldrBRD = document.getElementsByClassName("slider-board"); 
+ 
+
+console.log(sldrBRD[cur]);
+
+
 
 gesturedZone.addEventListener('touchstart', function(event) {
     touchstartX = event.changedTouches[0].screenX;
     touchstartY = event.changedTouches[0].screenY;
+    document.querySelector('[data-state=open]').classList.add('pop');
     event.preventDefault();
-    console.log(board);
-//     board.classList.toggle("pop");
 }, false);
 
 gesturedZone.addEventListener('touchend', function(event) {
     touchendX = event.changedTouches[0].screenX;
     touchendY = event.changedTouches[0].screenY;
     event.preventDefault();
-//     board.classList.toggle("pop");
-    console.log("farts");
+//     console.log('about to call handle gest');
     handleGesture();
 }, false); 
 
@@ -35,10 +36,10 @@ function handleGesture() {
       var distY = touchendY - touchstartY;
       var threshold = 45;
       var restraint = 10;
-      console.log(Math.abs(distX),Math.abs(distY),board.src);
+      // console.log(Math.abs(distX),Math.abs(distY));
       
        if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
-             console.log("first if");
+            //  console.log("first if");
                 swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
             }
             else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) {// 2nd condition for vertical swipe met
@@ -46,20 +47,38 @@ function handleGesture() {
             swipedir = (distY < 0)? 'up' : 'down' }// if dist traveled is negative, i
            console.log(swipedir);
       if(swipedir ==='right'){
-            cur =(cur +1) % productSliderSRC.length;
-            board.src = productSliderSRC[cur] ;
-            console.log("zany");
+            if (cur !== 0){
+            cur =(cur - 1) ;}
+            // if sldrbrd cur = open set to closed
+            sldrBRD[cur].setAttribute('data-state','open');
+            for(var i=0;i <= sldrBRD.length;i++){
+            if (sldrBRD[i] !== sldrBRD[cur]){
+            sldrBRD[i].setAttribute('data-state','closed');
+     document.querySelector('[data-state=open]').classList.remove('pop');
+            }}
+            console.log(sldrBRD[cur]);
             console.log(cur);
-            console.log(board.src);
       }
       else if (swipedir ==='left'){
-            cur=(cur -1) % productSliderSRC.length;
-           board.src = productSliderSRC[cur];
+            if(cur > sldrBRD.length){
+                  cur = 0
+            }else{
+            ++cur;}
+            console.log(cur);
+            sldrBRD[cur].setAttribute('data-state','open');
+             for(var i=0;i <= sldrBRD.length;i++){
+                   if(sldrBRD[i] !== sldrBRD[cur]){
+             sldrBRD[i].setAttribute('data-state','closed');
+     document.querySelector('[data-state=open]').classList.remove('pop');
+             }}
+             console.log(sldrBRD[cur]);
+    
+           
            console.log("tothemax");
-           console.log(cur);
-           console.log(board.src);
+      //     console.log(prevL);
       }
     
+
 //     var swiped = 'swiped: ';
 //     if (touchendX < touchstartX) {
       //   alert(swiped + 'left!');
